84088305e1f5fdd89630a7b334ed58c99b785751
esanolad <esanolad@gmail.com>
2022-08-08 15:00:31 +0100

    initial

---
+++ potential.py
@@ -0,0 +1,10 @@
+
+def look_for_influence(keyWord, start_date, end_date):
+    """
+    :param keyWord: search string to use in searching
+    :param start_date: beginning of date to use
+    :param end_date: end date to us
+    :return:
+    """
+
+    pass
